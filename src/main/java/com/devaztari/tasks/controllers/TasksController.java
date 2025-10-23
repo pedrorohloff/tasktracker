89f500005deb3e47f6f50e91f760d62aa705e4c9
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2025-10-23 19:51:04 -0300

    feat: added endpoint to update a Task inside a TaskList

--- src/main/java/com/devaztari/tasks/controllers/TasksController.java
+++ src/main/java/com/devaztari/tasks/controllers/TasksController.java
@@ -50,4 +50,18 @@ public class TasksController {
     ) {
         return taskService.getTask(taskListId, taskId).map(taskMapper::toDto);
     }
+
+    @PutMapping(path = "/{task_id}")
+    public TaskDto updateTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId,
+            @RequestBody TaskDto taskDto
+    ) {
+        Task updatedTask = taskService.updateTask(
+                taskListId,
+                taskId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(updatedTask);
+    }
 }
