bdc0f0121c745809a2db442e38322846700c6b80
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2025-10-23 19:24:52 -0300

    feat: added endpoint to get a single Task inside TaskList

--- src/main/java/com/devaztari/tasks/controllers/TasksController.java
+++ src/main/java/com/devaztari/tasks/controllers/TasksController.java
@@ -7,6 +7,7 @@ import com.devaztari.tasks.services.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -40,5 +41,13 @@ public class TasksController {
                 taskMapper.fromDto(taskDto)
         );
         return taskMapper.toDto(createdTask);
+    }
+
+    @GetMapping(path = "/{task_id}")
+    public Optional<TaskDto> getTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId
+    ) {
+        return taskService.getTask(taskListId, taskId).map(taskMapper::toDto);
     }
 }
--- src/main/java/com/devaztari/tasks/services/TaskService.java
+++ src/main/java/com/devaztari/tasks/services/TaskService.java
@@ -3,9 +3,11 @@ package com.devaztari.tasks.services;
 import com.devaztari.tasks.domain.entities.Task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
