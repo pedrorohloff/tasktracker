1d85cc7f21d0569b5f1db9283cc166aa0f6729ff
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2025-10-23 17:02:35 -0300

    feat: added services and repositories of Task and TaskList

---
+++ src/main/java/com/devaztari/tasks/services/TaskListService.java
@@ -0,0 +1,10 @@
+package com.devaztari.tasks.services;
+
+import com.devaztari.tasks.domain.entities.TaskList;
+
+import java.util.List;
+
+public interface TaskListService {
+    List<TaskList> listTaskLists();
+    TaskList createTaskList(TaskList taskList);
+}
---
+++ src/main/java/com/devaztari/tasks/services/impl/TaskListServiceImpl.java
@@ -0,0 +1,45 @@
+package com.devaztari.tasks.services.impl;
+
+import com.devaztari.tasks.domain.entities.TaskList;
+import com.devaztari.tasks.repositories.TaskListRepository;
+import com.devaztari.tasks.services.TaskListService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TaskListServiceImpl implements TaskListService {
+
+    private final TaskListRepository taskListRepository;
+
+    public TaskListServiceImpl(TaskListRepository taskListRepository) {
+        this.taskListRepository = taskListRepository;
+    }
+
+    @Override
+    public List<TaskList> listTaskLists() {
+        return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if (null != taskList.getId()) {
+            throw new IllegalArgumentException("Task list already has an ID.");
+        }
+        if(null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task list title must be present.");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskListRepository.save(new TaskList(
+                null,
+                taskList.getTitle(),
+                taskList.getDescription(),
+                null,
+                now,
+                now
+        ));
+    }
+}
