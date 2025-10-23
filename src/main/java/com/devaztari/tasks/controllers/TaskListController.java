400ae7b0fac86791a9722d726b4abe67bd424357
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2025-10-23 17:40:02 -0300

    feat: added endpoint to get TaskList by ID

--- src/main/java/com/devaztari/tasks/controllers/TaskListController.java
+++ src/main/java/com/devaztari/tasks/controllers/TaskListController.java
@@ -7,6 +7,8 @@ import com.devaztari.tasks.services.TaskListService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/task-lists")
@@ -36,4 +38,8 @@ public class TaskListController {
         return taskListMapper.toDto(createdTaskList);
     }
 
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId) {
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
 }
--- src/main/java/com/devaztari/tasks/services/TaskListService.java
+++ src/main/java/com/devaztari/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.devaztari.tasks.services;
 import com.devaztari.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
