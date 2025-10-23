c7f43503ca1e5dc6c5dd852c6ecb853b3612d28c
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2025-10-23 17:06:38 -0300

    chore: added auto-generated files from spring-initializr

---
+++ src/test/java/com/devaztari/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devaztari.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
